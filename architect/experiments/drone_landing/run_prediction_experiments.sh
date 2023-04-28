50ac5c373dd3c61f417956fe8301966a3b1ff081
Charles Dawson <charles.dwsn@gmail.com>
2023-04-28 12:00:48 -0400

    Update prediction scripts

--- architect/experiments/drone_landing/run_prediction_experiments.sh
+++ architect/experiments/drone_landing/run_prediction_experiments.sh
@@ -22,10 +22,8 @@ CUDA_VISIBLE_DEVICES=2, XLA_PYTHON_CLIENT_MEM_FRACTION=0.4 python architect/expe
 # Run REINFORCE/L2C prediction (with tempering)
 CUDA_VISIBLE_DEVICES=0, XLA_PYTHON_CLIENT_MEM_FRACTION=0.3 python architect/experiments/drone_landing/predict_and_mitigate.py --model_path tmp/oracle_32/policy_50.eqx --num_rounds 100 --num_steps_per_round 10 --num_chains 10 --T 60 --ep_mcmc_step_size 1e-3 --dp_mcmc_step_size 1e-3 --num_trees 10 --reinforce --savename drone_landing &
 
-# TODO
-
 # Run REINFORCE/L2C prediction (without tempering)
 CUDA_VISIBLE_DEVICES=1, XLA_PYTHON_CLIENT_MEM_FRACTION=0.4 python architect/experiments/drone_landing/predict_and_mitigate.py --model_path tmp/oracle_32/policy_50.eqx --num_rounds 100 --num_steps_per_round 10 --num_chains 10 --T 60 --ep_mcmc_step_size 1e-3 --dp_mcmc_step_size 1e-3 --num_trees 10 --reinforce --no-temper --savename drone_landing &
 
 # Run GD prediction (without tempering)
-CUDA_VISIBLE_DEVICES=1, XLA_PYTHON_CLIENT_MEM_FRACTION=0.4 python architect/experiments/drone_landing/predict_and_mitigate.py --model_path tmp/oracle_32/policy_50.eqx --num_rounds 100 --num_steps_per_round 10 --num_chains 10 --T 60 --ep_mcmc_step_size 1e-3 --dp_mcmc_step_size 1e-3 --num_trees 10 --disable_stochasticity --no-temper --savename drone_landing &
+CUDA_VISIBLE_DEVICES=0, XLA_PYTHON_CLIENT_MEM_FRACTION=0.4 python architect/experiments/drone_landing/predict_and_mitigate.py --model_path tmp/oracle_32/policy_50.eqx --num_rounds 100 --num_steps_per_round 10 --num_chains 10 --T 60 --ep_mcmc_step_size 1e-3 --dp_mcmc_step_size 1e-3 --num_trees 10 --disable_stochasticity --no-temper --savename drone_landing &
