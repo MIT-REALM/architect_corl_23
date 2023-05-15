4bf588e7d2182aff726a9e5657f7f69a5e1ca96e
Charles Dawson <charles.dwsn@gmail.com>
2023-05-14 22:32:18 -0400

    Clean up prediction stress test script

--- architect/experiments/drone_landing/prediction_stress_test.py
+++ architect/experiments/drone_landing/prediction_stress_test.py
@@ -25,13 +25,7 @@ BATCHES = 10
 # should we re-run the analysis (True) or just load the previously-saved summary (False)
 REANALYZE = False
 # path to save summary data to in predict_repair folder
-SUMMARY_PATH = (
-    "results/drone_landing_smooth/predict_repair/coverage_summary_seed_0.json"
-)
-# path to load convergence data from
-CONVERGENCE_SUMMARY_PATH = (
-    "results/drone_landing_smooth/predict/convergence_summary_gradnorm_mcmc_1e-2.json"
-)
+SUMMARY_PATH = "results/drone_landing_smooth/predict_repair/stress_test_seed_0.json"
 # Define data sources from individual experiments
 SEEDS = [0, 1, 2, 3]
 DATA_SOURCES = {
@@ -96,9 +90,8 @@ def load_data_sources_from_json():
     return loaded_data
 
 
-def monte_carlo_test(N, batches, loaded_data, seed):
+def monte_carlo_test(N, batches, loaded_data, alg, seed):
     """Stress test the given policy using N samples in batches"""
-    alg = "mala_tempered"
     image_shape = (32, 32)
     env = make_drone_landing_env(image_shape, loaded_data[alg][seed]["num_trees"])
     cost_fn = lambda ep: simulate(
@@ -127,47 +120,26 @@ if __name__ == "__main__":
     # Activate seaborn styling
     sns.set_theme(context="paper", style="whitegrid")
 
-    # Load convergence data (has costs for each algorithm)
-    with open(CONVERGENCE_SUMMARY_PATH, "rb") as f:
-        convergence_summary_data = json.load(f)
-
-        for alg in convergence_summary_data:
-            for result in convergence_summary_data[alg]:
-                result["ep_costs"] = jnp.array(result["ep_costs"])
-                result["ep_logpriors"] = jnp.array(result["ep_logpriors"])
-                result["ep_logprobs"] = jnp.array(result["ep_logprobs"])
-
     if REANALYZE or not os.path.exists(SUMMARY_PATH):
         # Load the data
         data = load_data_sources_from_json()
 
-        # Compute costs (Change seed no. for different seeds)
-        monte_carlo_costs = monte_carlo_test(N, BATCHES, data, 0)
+        # Compute costs for each seed for each algorithm
+        for alg in data:
+            for seed, result in enumerate(data[alg]):
+                print(f"Computing costs for {alg} seed {seed}")
+                result["costs"] = monte_carlo_test(N, BATCHES, data, alg, seed)
 
         # Extract the summary data we want to save
-        summary_data = {
-            "random_sample_costs": monte_carlo_costs,
-            "N": N,
-            "batches": BATCHES,
-        }
-        for alg in convergence_summary_data:
+        summary_data = {}
+        for alg in data:
             summary_data[alg] = []
             for seed in SEEDS:
                 summary_data[alg].append(
                     {
-                        "display_name": convergence_summary_data[alg][seed][
-                            "display_name"
-                        ],
-                        "ep_costs": convergence_summary_data[alg][seed]["ep_costs"],
-                        "ep_logpriors": convergence_summary_data[alg][seed][
-                            "ep_logpriors"
-                        ],
-                        "ep_logprobs": convergence_summary_data[alg][seed][
-                            "ep_logprobs"
-                        ],
-                        "failure_level": convergence_summary_data[alg][seed][
-                            "failure_level"
-                        ],
+                        "display_name": data[alg][seed]["display_name"],
+                        "costs": data[alg][seed]["costs"],
+                        "failure_level": data[alg][seed]["failure_level"],
                     }
                 )
 
@@ -185,22 +157,12 @@ if __name__ == "__main__":
         with open(SUMMARY_PATH, "rb") as f:
             summary_data = json.load(f)
 
-            summary_data["random_sample_costs"] = jnp.array(
-                summary_data["random_sample_costs"]
-            )
             for alg in DATA_SOURCES:
                 for result in summary_data[alg]:
-                    result["ep_costs"] = jnp.array(result["ep_costs"])
-                    result["ep_logpriors"] = jnp.array(result["ep_logpriors"])
-                    result["ep_logprobs"] = jnp.array(result["ep_logprobs"])
+                    result["costs"] = jnp.array(result["costs"])
 
     # Post-process into a dataframe
-    df = pd.DataFrame(
-        {
-            "Algorithm": "Monte Carlo from prior",
-            "Cost": summary_data["random_sample_costs"],
-        }
-    )
+    df = pd.DataFrame()
     for alg in DATA_SOURCES:
         for result in summary_data[alg]:
             df = df.append(
@@ -211,54 +173,6 @@ if __name__ == "__main__":
                     }
                 )
             )
-
-    # Compute wassertein distance of each algorithm from the prior
-    bins = 10
-    gt, gt_bins = jnp.histogram(
-        summary_data["random_sample_costs"],
-        bins,
-        density=True,
-        weights=jnp.exp(-jax.nn.elu(25.0 - summary_data["random_sample_costs"])),
-    )
-    gt_bin_centers = (gt_bins[1:] + gt_bins[:-1]) / 2.0
-    print(
-        "Wasserstein distance from ground truth (importance sampling w. "
-        + f"N={summary_data['N'] * summary_data['batches']}):"
-    )
-    for alg in DATA_SOURCES:
-        costs = jnp.concatenate(
-            [x["ep_costs"][5:].reshape(-1) for x in summary_data[alg]]
-        )
-        hist, hist_bins = jnp.histogram(costs, bins, density=True)
-        hist_bin_centers = (hist_bins[1:] + hist_bins[:-1]) / 2.0
-
-        distance = lambda x, y: jnp.linalg.norm(x - y)
-        distance_to_gt = lambda x: jax.vmap(distance, in_axes=(0, None))(
-            gt_bin_centers, x
-        )
-        distance_matrix = jax.vmap(distance_to_gt)(hist_bin_centers)
-
-        wasserstein = pyemd.emd(
-            np.array(gt, dtype=np.float64),
-            np.array(hist, dtype=np.float64),
-            np.array(distance_matrix, dtype=np.float64),
-        )
-        print(f"{DATA_SOURCES[alg]['display_name']}: {wasserstein}")
-    
-    # Also compute and print the JS divergence
-    print(f"JS divergence from ground truth (importance sampling w. {N * BATCHES}):")
-    for alg in DATA_SOURCES:
-        costs = jnp.concatenate(
-            [x["ep_costs"][0:].reshape(-1) for x in summary_data[alg]]
-        )
-        # re-compute the histogram to use the same bins as the ground truth
-        hist, hist_bins = jnp.histogram(costs, bins, density=True)
-
-        # Convert to numpy
-        gt = np.array(gt, dtype=np.float64)
-        hist = np.array(hist, dtype=np.float64)
-        js = scipy.spatial.distance.jensenshannon(gt, hist)
-        print(f"{DATA_SOURCES[alg]['display_name']} JS: {js}")
 
     # Plot!
     plt.figure(figsize=(12, 8))
@@ -271,5 +185,5 @@ if __name__ == "__main__":
         data=df,
     )
     plt.gca().set_xlabel("")
-    #plt.savefig('results/drone_landing_smooth/predict_repair/seed_0.png') #saving images to file for each seed
+    # plt.savefig('results/drone_landing_smooth/predict_repair/seed_0.png') #saving images to file for each seed
     plt.show()
