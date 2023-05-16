93b96c975d381931d5b684c75e3f7423f550484d
Charles Dawson <charles.dwsn@gmail.com>
2023-05-16 09:12:19 -0400

    more repair experiments

---
+++ get_results_from_aws.sh
@@ -0,0 +1,2 @@
+rsync -azpv --exclude *trace* ubuntu@ec2-3-86-2-122.compute-1.amazonaws.com:/home/ubuntu/src/architect_private/results/* /home/cbd/src/mit/architect_private/results/
+rsync -azpv --exclude *trace* ubuntu@ec2-18-215-253-215.compute-1.amazonaws.com:/home/ubuntu/src/architect_private/results/* /home/cbd/src/mit/architect_private/results/
