d4214c2af1faa4a302cf85f7a72d5aea2ee13c3a
schannap <123418940+schannap@users.noreply.github.com>
2023-07-17 14:10:23 -0400

    Add n_targets attribute; Change distance calculation
    Distance calculation no longer considers theta. Change x_inits to have uniform distribution

--- architect/systems/turtle_bot/turtle_bot_game.py
+++ architect/systems/turtle_bot/turtle_bot_game.py
@@ -16,7 +16,7 @@ from architect.systems.components.dynamics.dubins import dubins_next_state
 from architect.systems.turtle_bot.turtle_bot_types import (
     Arena,
     Policy,
-    Environment_state,# get_sigma, get_concentration, get_target_pos,
+    Environment_state, #get_sigma, get_concentration, get_target_pos,
     TurtleBotResult
 )
 
@@ -29,32 +29,38 @@ class Game(eqx.Module):
     The turtle bot game engine
 
     args:
-        policy: provides control inputs
+        
         x_init: 
         x_hist:
         conc_hist:
+        memory_length:
         duration:
         dt:
+        x_init_spread: 
     """
-
-    policy: Policy
-    x_init: Float[Array, " n "]
-    x_hist: Float[Array, " n "]
-    conc_hist: Float[Array, " n "]
+    #TODO: added self before every "policy" & "exogenous_env"
+    #exogenous_env: Environment_state #TODO: this was added only to test the issue with predict_and_mitigate
+    #policy: Policy #TODO: this was added only to test the issue with predict_and_mitigate
+    x_inits: jnp.ndarray#Float[Array, " n "]
+    x_hists: jnp.ndarray#Float[Array, " n "]
+    conc_hists: jnp.ndarray#Float[Array, "memory_length"]
     memory_length: int
+    n_targets: int
     duration: float = 100.0
     dt: float = 0.1
-    x_init_spread = 1 # static variable for x_init spread
+    #x_init_spread = 1 # static variable for x_init spread
+
     @jaxtyped
     @beartype
     def __call__(
         self,
-        policy: Policy,
-        x_init: Float[Array, " n "],
-        x_hist: Float[Array, " n "],
-        conc_hist: Float[Array, " n "], 
-        exogenous_env: Environment_state,
-        arena: Arena
+        
+        target_pos: jnp.ndarray,
+        sigma: jnp.ndarray,
+        x_inits: jnp.ndarray,
+        policy: Policy, #TODO: this was removed only to test the issue with predict_and_mitigate
+        #exogenous_env: Environment_state, #TODO: this was removed only to test the issue with predict_and_mitigate
+        
     ) -> TurtleBotResult:
         """
         Play out a game of turtle bots.
@@ -63,26 +69,31 @@ class Game(eqx.Module):
 
         """
         T = self.duration
+        exogenous_env = Environment_state(target_pos, sigma, x_inits)
         # Define a function to execute one step in the game
 
         def step_policy(carry, dummy_input): 
+            #print (carry, "carry")
+            #print (len(carry), "shape of carry")
+            #print (jnp.shape(carry[0]), "shape in carry")
             x_hist, conc_hist, x_current = carry
+            #print (x_hist.shape, "SHAPE in step_policy")
             control_inputs = jax.nn.tanh(policy(x_hist, conc_hist)) 
             # finding the next state (x, y, theta)
-            x_next = dubins_next_state(x_current, control_inputs, self.dt) 
+            x_next = dubins_next_state(x_current, control_inputs, actuation_noise = jnp.array([0.0,0.0,0.0]), dt=0.1) 
             jnp.delete(x_hist, -1, 0)
             jnp.delete(conc_hist, -1, 0)
             # insert the next state into the beginning of the list of position history
             jnp.insert(x_hist, 0, x_next, axis=0)
             # insert the newest concentration found at x_next
-            jnp.insert(conc_hist, 0, exogenous_env.get_concentration(x_next[0], x_next[1]), axis = 0)
+            jnp.insert(conc_hist, 0, exogenous_env.get_concentration(x_next[0], x_next[1], self.n_targets), axis = 0)
             return (x_hist, conc_hist, x_next), (x_next, control_inputs) 
         
         
 # =============================================================================
 # 
 #         prng_key = jax.random.PRNGKey(0)
-#         x_inits = self.x_init_spread * jax.random.normal(prng_key, shape=(self.N, 3)) 
+#         x_inits = self.x_init_spread * jax.random.uniform(prng_key, shape=(self.N, 3)) 
 #         
 #         def make_x_hist(x_init):
 #             x_hist = jnp.array([x_init for i in range(self.memory_length)])
@@ -110,28 +121,33 @@ class Game(eqx.Module):
 #         cost = sum(cost) + controls.mean() # TODO: THIS MIGHT NOT WORK WITH CONTROLS.MEAN()
 #         
 # =============================================================================
+        #print (self.x_hists.shape, "SHAPE")
+        test_carry = (self.x_hists, self.conc_hists, self.x_inits)
+        #print (jnp.shape(test_carry[0]), "TEST CARRY")
         simulate_scan = lambda x_init, x_hist, conc_hist: jax.lax.scan(step_policy, (x_hist, conc_hist, x_init), xs=None, length=T)[1]
-        simulate_scan = jax.vmap(simulate_scan)
-        trajectory, control = simulate_scan(x_init, x_hist, conc_hist)
+        simulate_batched = jax.vmap(simulate_scan)
+        trajectory, control = simulate_batched(self.x_inits, self.x_hists, self.conc_hists)
         control = control**2
         
-        def get_squared_distance(trajectory, target: Float[Array, "n 2"]):
-            distance = ((trajectory[:,:,0]-target[:,:,0])**2 + (trajectory[:,:,1]-target[:,:,1])**2+(trajectory[:,:,2]-target[:,:,2])**2)
-            return distance
         
+        
+                
+        def get_squared_distance(trajectory, target: Float[Array, "n 2"]):
+            distance = ((trajectory[:,:,0]-target[:,:,0])**2 + (trajectory[:,:,1]-target[:,:,1])**2)
+            return distance
         # find a way to find distances without knowing how many targets there are  
         loss = lambda sigma, distance: -1/sigma*jax.lax.exp(-distance/sigma).mean()
-        cost = [loss(exogenous_env.get_sigma()[n], get_squared_distance(trajectory, (jnp.zeros_like(trajectory)+ exogenous_env.get_target_pos()[n]))) for n in range(arena.get_n_targets())]
-        cost = sum(cost) + control.mean()
 
+        cost = [loss(exogenous_env.get_sigma()[n], get_squared_distance(trajectory, (jnp.zeros_like(trajectory[:,:,:2])+ target_pos[n]))) for n in range(self.n_targets)]
+        cost = sum(cost) + control.mean()
+        
         
 
-#TODO: define x_inits as an exogenous parameter
 
         return TurtleBotResult(
-            n_targets=Arena.get_n_targets(),
-            target_pos=get_target_pos(),
-            sigma=get_sigma(),
+            n_targets=self.n_targets,
+            target_pos=exogenous_env.get_target_pos(),
+            sigma=exogenous_env.get_sigma(),
             game_duration=self.duration,
             squared_controls=control,
             potential=cost,
