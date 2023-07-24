69cd2194fc1841a37de70fc6311df657d110db54
schannap <123418940+schannap@users.noreply.github.com>
2023-07-24 13:59:00 -0400

    Add log_smooth_uniform method

--- architect/utils.py
+++ architect/utils.py
@@ -2,6 +2,7 @@
 from beartype import beartype
 from jax.nn import logsumexp
 from jaxtyping import Array, Float, jaxtyped
+from jax.nn import log_sigmoid
 
 
 @jaxtyped
@@ -16,3 +17,10 @@ def softmax(x: Float[Array, "..."], sharpness: float = 0.05):
 def softmin(x: Float[Array, "..."], sharpness: float = 0.05):
     """Return the soft minimum of the given vector"""
     return -softmax(-x, sharpness)
+
+
+@jaxtyped
+@beartype
+def log_smooth_uniform(x, x_min, x_max, smoothing):
+    """Return the smooth approximation of uniform distribution for log probabilities"""
+    return log_sigmoid(smoothing * (x - x_min)) + log_sigmoid(smoothing * (x_max - x))
