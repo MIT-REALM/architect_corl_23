ef2a2914353e85a76b42d9f3da3073fdf7cf2bae
schannap <123418940+schannap@users.noreply.github.com>
2023-07-25 15:30:52 -0400

    Change simulator call method
    Create and update history arrays in call method and step_policy respectively, convert logsigma to sigma

--- architect/systems/turtle_bot/turtle_bot_game.py
+++ architect/systems/turtle_bot/turtle_bot_game.py
@@ -29,17 +29,12 @@ class Game(eqx.Module):
     The turtle bot game engine
 
     args:
-        x_inits: turtle-bot's starting positions
-        x_hists: array of turtle-bot's past positions
-        conc_hists: array of turtle-bot's past concentration values
         memory_length: number of past states remembered
+        n_targets: number of sources/targets in the environment
         duration: length of game (seconds)
         dt: timestep of the game's simulation (seconds)
     """
 
-    x_inits: jnp.ndarray
-    x_hists: jnp.ndarray
-    conc_hists: jnp.ndarray
     memory_length: int
     n_targets: int
     duration: float = 100.0
@@ -50,9 +45,9 @@ class Game(eqx.Module):
     def __call__(
         self,
         
-        target_pos: jnp.ndarray,
-        sigma: jnp.ndarray,
-        x_inits: jnp.ndarray,
+        target_pos: Float[Array, "n_targets 2"],
+        sigma: Float[Array, "n_targets "],
+        x_inits: Float[Array, "N 3"],
         policy: Policy, 
         
     ) -> TurtleBotResult:
@@ -66,8 +61,24 @@ class Game(eqx.Module):
             policy: neural network policy used to for control input
 
         """
+        e_sigma = jax.exp(sigma)
         T = self.duration
-        exogenous_env = Environment_state(target_pos, sigma, x_inits)
+        exogenous_env = Environment_state(target_pos, e_sigma, x_inits)
+        # initialize x_hists
+        def make_x_hist(x_init):
+            x_hist = jnp.array([x_init for i in range(self.memory_length)])
+            return x_hist
+        make_x_hists = jax.vmap(make_x_hist)
+        x_hists = make_x_hists(x_inits)
+
+         # initialize conc_hists
+        def make_conc_hist(x_init):
+            conc = exogenous_env.get_concentration(x_init[0], x_init[1], self.n_targets)
+            conc_hist = jnp.array([conc for i in range(self.memory_length)])
+            return conc_hist
+        make_conc_hists = jax.vmap(make_conc_hist)
+        conc_hists = make_conc_hists(x_inits)
+        
         # Define a function to execute one step in the game
 
         def step_policy(carry, dummy_input): 
@@ -75,18 +86,18 @@ class Game(eqx.Module):
             control_inputs = jax.nn.tanh(policy(x_hist, conc_hist)) 
             # finding the next state (x, y, theta)
             x_next = dubins_next_state(x_current, control_inputs, actuation_noise = jnp.array([0.0,0.0,0.0]), dt=0.1) 
-            jnp.delete(x_hist, -1, 0)
-            jnp.delete(conc_hist, -1, 0)
+            x_hist = jnp.delete(x_hist, -1, 0)
+            conc_hist = jnp.delete(conc_hist, -1, 0)
             # insert the next state into the beginning of the list of position history
-            jnp.insert(x_hist, 0, x_next, axis=0)
+            x_hist = jnp.insert(x_hist, 0, x_next, axis=0)
             # insert the newest concentration found at x_next
-            jnp.insert(conc_hist, 0, exogenous_env.get_concentration(x_next[0], x_next[1], self.n_targets), axis = 0)
+            conc_hist = jnp.insert(conc_hist, 0, exogenous_env.get_concentration(x_next[0], x_next[1], self.n_targets), axis = 0)
             return (x_hist, conc_hist, x_next), (x_next, control_inputs) 
         
         
         simulate_scan = lambda x_init, x_hist, conc_hist: jax.lax.scan(step_policy, (x_hist, conc_hist, x_init), xs=None, length=T)[1]
         simulate_batched = jax.vmap(simulate_scan)
-        trajectory, control = simulate_batched(self.x_inits, self.x_hists, self.conc_hists)
+        trajectory, control = simulate_batched(x_inits, x_hists, conc_hists)
         control = control**2
         
         
@@ -95,7 +106,7 @@ class Game(eqx.Module):
             return distance
         # find distances without knowing how many targets there are  
         loss = lambda sigma, distance: -1/sigma*jax.lax.exp(-distance/sigma).mean()
-        cost = [loss(exogenous_env.get_sigma()[n], get_squared_distance(trajectory, (jnp.zeros_like(trajectory[:,:,:2])+ target_pos[n]))) for n in range(self.n_targets)]
+        cost = [loss(e_sigma[n], get_squared_distance(trajectory, (jnp.zeros_like(trajectory[:,:,:2])+ target_pos[n]))) for n in range(self.n_targets)]
         cost = sum(cost) + control.mean()
         
 
