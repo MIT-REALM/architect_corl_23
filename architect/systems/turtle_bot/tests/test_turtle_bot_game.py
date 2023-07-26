bebd11aa7884385201154b1f6c24ed4f84652608
schannap <123418940+schannap@users.noreply.github.com>
2023-07-26 15:33:02 -0400

    Fix typos and update plotting
    Use get_concentration from EnvironmentState for plotting

--- architect/systems/turtle_bot/tests/test_turtle_bot_game.py
+++ architect/systems/turtle_bot/tests/test_turtle_bot_game.py
@@ -16,35 +16,50 @@ from architect.systems.components.dynamics.dubins import dubins_next_state
 from architect.systems.turtle_bot.turtle_bot_types import (
     Arena,
     Policy,
-    Environment_state, 
+    EnvironmentState, 
     TurtleBotResult
 )
 from architect.systems.turtle_bot.turtle_bot_game import Game
 import matplotlib.pyplot as plt
 
 def test_Game(plot=False):
-    
+    """
+    Tests the Game class in turtle_bot_game. 
+    Computes the result of two games called with different initial conditions and/or memory lengths.
+    Checks that the resulting controls, potential (loss), and trajectories have the correct shapes.
+    If plotting is enabled, the scenarios will be plotted on a contour plot.
+    """
     # Run this function if plots are requested
-    def make_plot(result):    
-        def get_concentration(x: float, y: float):
-            concentration: float = 0
-            targets = target_pos
-            sigmas = sigma
-            make_gaussian = lambda target, sig: 1/sig*jax.lax.exp(-1/sig* ((target[0]-x)**2 + (target[1]-y)**2))
-            concs = [make_gaussian(targets[n], sigmas[n]) for n in range(num_targets)]
-            concentration = sum(concs)
-            concentration = jnp.take(concentration, 0)
-            return concentration
+    def make_plot(result, environment, n_targets):    
+        # def get_concentration(x: float, y: float):
+        #     concentration: float = 0
+        #     targets = target_pos
+        #     sigmas = sigma
+        #     make_gaussian = lambda target, sigma: jax.scipy.stats.norm.pdf(
+        #     ((target[0] - x) ** 2 + (target[1] - y) ** 2), target, sigma
+        # )
+        #     #make_gaussian = lambda target, sig: 1/sig*jax.lax.exp(-1/sig* ((target[0]-x)**2 + (target[1]-y)**2))
+        #     concs = [make_gaussian(targets[n], sigmas[n]) for n in range(num_targets)]
+        #     concentration = sum(concs)
+        #     concentration = jnp.take(concentration, 0)
+        #     return concentration
+        
+        def dummy_fn(x, y):
+            return environment.get_concentration(x, y, n_targets)
                 
         delta = 0.025
         
         x = jnp.arange(-3.0, 3.0, delta)
         y = jnp.arange(-2.0, 2.0, delta)
         X, Y = jnp.meshgrid(x, y)
-                
-        data = jax.vmap(jax.vmap(get_concentration))(X,Y)
+       
+        
+        #data = (jax.vmap(jax.vmap(get_concentration))(X,Y))
+        data = jax.vmap(jax.vmap(dummy_fn))(X,Y)
+        #data = (jax.vmap(environment.get_concentration, in_axes = (0,0,None)))(X,Y,1)
+        
         plt.figure()
-        plt.contour(X,Y, data)
+        plt.contour(X,Y,data)
         for i in range(N):
             plt.plot(result.xs[i,:,0], result.xs[i,:,1])
             plt.plot(result.xs[i,0,0], result.xs[i,0,1], "bo")
@@ -57,7 +72,7 @@ def test_Game(plot=False):
     
     # For a memory length of 1 and 1 target position
     # Create policy
-    memory_length = 1
+    memory_length = 2
     prng_key = jax.random.PRNGKey(1)
     prng_key, subkey = jax.random.split(prng_key)
     policy = Policy(subkey,memory_length)
@@ -67,27 +82,10 @@ def test_Game(plot=False):
     prng_key = jax.random.PRNGKey(0)
     x_inits = 1 * jax.random.uniform(prng_key, shape=(N, 3), minval=-1.0, maxval=1.0) 
     
-    # Create x_hists
-    def make_x_hist(x_init):
-        x_hist = jnp.array([x_init for i in range(memory_length)])
-        return x_hist
-    make_x_hists = jax.vmap(make_x_hist)
-    x_hists = make_x_hists(x_inits)
-    
-    # Create conc_hists
-    def make_conc_hist(x_init):
-        conc = 0.05*x_init[0]
-        conc_hist = jnp.array([conc for i in range(memory_length)])
-        return conc_hist
-    make_conc_hists = jax.vmap(make_conc_hist)
-    conc_hists = make_conc_hists(x_inits)
     
     # Create the game
     game = Game(
-        x_inits,
-        x_hists,
-        conc_hists,
-        memory_length = 1,
+        memory_length = 2,
         n_targets = 1,
         duration = 100.0,
         dt = 0.1
@@ -98,18 +96,23 @@ def test_Game(plot=False):
     num_targets = target_pos.shape[0]
     # Define test sigma
     sigma = jnp.array([0.6])
+    
+    env = EnvironmentState(target_pos, sigma, x_inits)
+    
     # Evaluate the game
     result = game(target_pos, sigma, x_inits, policy)
+    assert result.squared_controls.shape == (N, game.duration, 2)
+    assert result.potential.shape == ()
+    assert result.xs.shape == (N, game.duration, 3)
     if plot:
-        make_plot(result)
-    
+        make_plot(result, env, num_targets)
     
     
     
     # For a memory_length of 4 and 2 target positions
     # Create policy
     memory_length = 4
-    prng_key = jax.random.PRNGKey(1)
+    prng_key = jax.random.PRNGKey(2)
     prng_key, subkey = jax.random.split(prng_key)
     policy = Policy(subkey,memory_length)
     
@@ -117,28 +120,11 @@ def test_Game(plot=False):
     N=20
     prng_key = jax.random.PRNGKey(0)
     x_inits = 2 * jax.random.uniform(prng_key, shape=(N, 3), minval=-1.0, maxval=1.0) 
-    
-    # Create x_hists
-    def make_x_hist(x_init):
-        x_hist = jnp.array([x_init for i in range(memory_length)])
-        return x_hist
-    make_x_hists = jax.vmap(make_x_hist)
-    x_hists = make_x_hists(x_inits)
-    
-    # Create conc_hists
-    def make_conc_hist(x_init):
-        conc = 0.05*x_init[0]
-        conc_hist = jnp.array([conc for i in range(memory_length)])
-        return conc_hist
-    make_conc_hists = jax.vmap(make_conc_hist)
-    conc_hists = make_conc_hists(x_inits)
+
     
     # Create the game
     game = Game(
-        x_inits,
-        x_hists,
-        conc_hists,
-        memory_length = 1,
+        memory_length = 4,
         n_targets = 1,
         duration = 100.0,
         dt = 0.1
@@ -149,11 +135,17 @@ def test_Game(plot=False):
     num_targets = target_pos.shape[0]
     # Define test sigma
     sigma = jnp.array([0.6, 0.8])
+    
+    env = EnvironmentState(target_pos, sigma, x_inits)
+
     # Evaluate the game
     result = game(target_pos, sigma, x_inits, policy)
+    assert result.squared_controls.shape == (N, game.duration, 2)
+    assert result.potential.shape == ()
+    assert result.xs.shape == (N, game.duration, 3)
     if plot:
-        make_plot(result)
-    
+        make_plot(result, env, num_targets)
+
     
     plt.show()
 
